c922208d3bf2d6e4e81a534d8b4be2877b290e93
Joe <bluivy@163.com>
2026-08-05 00:12:52 +0800

    test

---
+++ script.py
@@ -0,0 +1,11 @@
+# Copy Dockerfile test
+cp ./movieinfo-service/Dockerfile ./artifact-catalog-service/
+cp ./movieinfo-service/Dockerfile ./ratingdata-service/
+
+# Copy deployment.yaml
+cp ./movieinfo-service/deployment.yaml ./artifact-catalog-service/
+cp ./movieinfo-service/deployment.yaml ./ratingdata-service/
+
+# Copy service.yaml test
+cp ./movieinfo-service/service.yaml ./artifact-catalog-service/
+cp ./movieinfo-service/service.yaml ./ratingdata-service/
