845415b41b9a0003d6d13d76d769f51bb9d47513
Joe <bluivy@163.com>
2026-08-07 02:48:53 +0800

    port number updated

--- artifact-catalog-service/src/main/java/com/example/artifact_catalog_service/DemoController.java
+++ artifact-catalog-service/src/main/java/com/example/artifact_catalog_service/DemoController.java
@@ -19,7 +19,7 @@ public class DemoController {
     private final WebClient webClient;
     
     public DemoController(WebClient.Builder webBuilder) {
-        this.webClient = webBuilder.baseUrl("http://movieinfo-service:80").build();
+        this.webClient = webBuilder.baseUrl("http://movieinfo-service:8081").build();
     }
     
     @RequestMapping("/{userID}")
